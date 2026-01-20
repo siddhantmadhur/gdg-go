88e1cb1c8d7b7f84f03ab4ca52c34208d22b3bb4
Siddhant Madhur <smadhur@reocelot.com>
2026-01-20 14:16:13 -0800

    removed sol

--- multithreading/multithreading.go
+++ multithreading/multithreading.go
@@ -2,28 +2,20 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
 func main() {
 	var sum int = 0
 
-	var wg sync.WaitGroup
-	var m sync.Mutex
 	for range 5 {
-		wg.Add(1)
-		go add(&sum, &wg, &m)
+		add(&sum)
 	}
-	wg.Wait()
 
 	fmt.Printf("Sum: %d\n", sum)
 }
 
-func add(sum *int, wg *sync.WaitGroup, m *sync.Mutex) {
-	defer wg.Done()
+func add(sum *int) {
 	time.Sleep(time.Second)
-	m.Lock()
 	*sum += 1
-	m.Unlock()
 }
