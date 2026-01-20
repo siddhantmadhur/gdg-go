8292ebbe3fa3871b1697fb60d61319c3553906cb
Siddhant Madhur <smadhur@reocelot.com>
2026-01-20 13:24:30 -0800

    added roulette

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gdg
+
+go 1.25.5
---
+++ roulette.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math/rand/v2"
+	"os"
+)
+
+func main() {
+	val, err := russianRoulette(6)
+	if err != nil {
+		fmt.Printf("There was an error: %s\n", err.Error())
+		os.Exit(1)
+	}
+	fmt.Printf("Safe with val %d\n", val)
+}
+
+func russianRoulette(bullet int) (int, error) {
+	if bullet >= 6 {
+		return -1, errors.New("Bullet has to be between 0 and 5")
+	}
+	val := rand.Int() % 6
+	fmt.Printf("Got: %d\n", val)
+	if val == bullet {
+		return val, errors.New("Lost!")
+	}
+	return val, nil
+}
