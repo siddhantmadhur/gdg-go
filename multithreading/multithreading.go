fb8b9636fcbceecfa6b1027578cf354966abd43c
Siddhant Madhur <smadhur@reocelot.com>
2026-01-20 13:52:19 -0800

    updated multi

--- multithreading/multithreading.go
+++ multithreading/multithreading.go
@@ -2,44 +2,20 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
-var store = make(map[string]int)
-
-var SLOW = true
-
 func main() {
-	go put("a", 120) // runs on a new thread
-	put("b", 250)
-	put("c", 140)
+	var sum int = 0
 
-	var a, b, c int
-
-	if SLOW {
-		get("a", &a)
-		get("b", &b)
-		get("c", &c)
-	} else {
-		var wg sync.WaitGroup
-		wg.Go(func() { get("a", &a) })
-		wg.Go(func() { get("b", &b) })
-		wg.Go(func() { get("c", &c) })
-		wg.Wait()
+	for range 5 {
+		add(&sum)
 	}
-
-	sum := a + b + c
 
 	fmt.Printf("Sum: %d\n", sum)
 }
 
-func put(key string, val int) {
-	fmt.Printf("Updated (%s:%d)\n", key, val)
-	store[key] = val
-}
-
-func get(key string, val *int) {
-	time.Sleep(time.Second * 2)
-	*val = store[key]
+func add(sum *int) {
+	time.Sleep(time.Second)
+	*sum += 1
 }
